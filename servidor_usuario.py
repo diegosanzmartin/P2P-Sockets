098eb1cc0499bf0e7cdb066ce13bf81994c9bc1c
diego-acc <diego.sm.acc@gmail.com>
2021-04-01 18:02:21 +0200

    Server v2.0 (comentarios + estética)

--- servidor_usuario.py
+++ servidor_usuario.py
@@ -5,15 +5,16 @@ import signal, select, socket, json, sys, os, re                                
 from time import time                                                           #Cronometrar tiempos
 from time import sleep
 
-ERR = "\033[93m"
+RED = "\033[93m"
 END = "\033[0m"
+BAR = "\n———————————————————————————————————————\n"
 
-expIPv4 = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
-expPort = "(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[0-5]?([0-9]){0,3}[0-9])"
+expIPv4 = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"  #Expresión regular dir IP
+expPort = "(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[0-5]?([0-9]){0,3}[0-9])"     #Expresión regular puerto
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 :                                                     #Esta función compara los tiempos para
-        print(ERR + "ERR: Nº de argumentos no válidos" + END)
+        print(RED + "ERR: Nº de argumentos no válidos" + END)
         sys.exit()
 
     #Variables socket
@@ -22,10 +23,10 @@ if __name__ == "__main__":
     serv_addr = (servIP, servPort)
 
     if servPort < 1023:                                                         #Comprobamos el nº de puerto
-        print(ERR + "ERR: El nº de puerto debe ser mayor que 1023" + END)
+        print(RED + "ERR: El nº de puerto debe ser mayor que 1023" + END)
         sys.exit()
 
-    #Creación del socket TCP
+    #Creación del socket servidor de direcciones peers
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setblocking(0)                                                         #Establecemos modo no bloqueo
 
@@ -33,9 +34,9 @@ if __name__ == "__main__":
     #Conexión socket
     sock.bind(serv_addr)
 
-    sock.listen(10)                                                             #El socket está a la escucha del cliente
-    inputs = [sock]                                                             #Lista de sockets de lectura
-    outputs = []                                                                #Lista de sockets de escritura
+    sock.listen(10)                                                             #El socket está a la escucha de clientes
+    inputs = [sock]                                                             #Lista de sockets de entrada
+    outputs = []                                                                #Lista de sockets de salida
 
     cliServ = {}
     cliServ["clientes"] = []
@@ -44,8 +45,8 @@ if __name__ == "__main__":
         try:
             readable, writable, exceptional = select.select(inputs, outputs, inputs)     #Select coordina entre i/o
 
-            for sck in readable:
-                if sck == sock:
+            for sck in readable:                                                #Sockets de entrada disponibles
+                if sck == sock:                                                 #Un nuevo socket desea conectarse
                     cli, cli_add = sock.accept()                                #Aceptamos la conexión de un socket "readable"
                     cli.setblocking(0)                                          #Establecemos modo no bloqueo
                     print("-Cliente:", cli_add, "conectado")
@@ -53,38 +54,43 @@ if __name__ == "__main__":
                     inputs.append(cli)                                          #Añadimos a la lista inputs
                     outputs.append(cli)                                         #Añadimos a la lista outputs
 
-                    data = str(json.dumps(cliServ))
-                    cli.send(data.encode())
+                    data = str(json.dumps(cliServ))                             #Convertimos JSON en string
+                    cli.send(data.encode())                                     #Enviamos lista codificada a el nuevo cliente
 
                 else:
                     cliServ_addr = sck.getpeername()
                     dataRecv = sck.recv(1024)
 
                     if dataRecv:
-                        dataRecv = str(dataRecv.strip().decode('utf-8'))
+                        dataRecv = str(dataRecv.strip().decode('utf-8'))        #Decodificamos la información recibida
                         
-                        addValido = re.compile(r"^\('"+ expIPv4 + "'," + expPort + "\)$")
+                        addValido = re.compile(r"^\('"+ expIPv4 + "'," + expPort + "\)$")   #Comprobamos que es una tupla ('IP', port)
 
                         if addValido != None:
+                            #Almacenamos la direccion de escucha del peer junto con el socket de conexión de este servidor
                             cliServ["clientes"].append({"peerServ_addr" : str(dataRecv), "cliServ_addr": str(cliServ_addr)})
-
-                            print(cliServ)
+                            print(BAR[:-30] + "JSON actualizado" + BAR[1:-29])
+                            print(RED ,cliServ, END)
+                            print(BAR)
 
                         else:
                             print(dataRecv)
 
                     else:
-                        outputs.remove(sck)
-                        inputs.remove(sck)
+                        inputs.remove(sck)                                      #Eliminamos el socket de la lista de inputs
+                        outputs.remove(sck)                                     #Eliminamos el socket de la lista de outputs
                         sck.close()
 
                         n = 0
-                        for p in cliServ["clientes"]:
+                        for p in cliServ["clientes"]:                           #Eliminamos del JSON
                             if p["cliServ_addr"] == str(cliServ_addr):
                                 cliServ["clientes"].pop(n)
                             n += 1
 
                         print("-Cliente:", cliServ_addr, "desconectado")
+                        print(BAR[:-29] + "JSON actualizado" + BAR[1:-28])
+                        print(RED , cliServ, END)
+                        print(BAR)
 
 
             for sck in exceptional:                                             #Condiciones excepcionales
